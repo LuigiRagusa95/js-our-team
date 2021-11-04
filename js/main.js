db61f1b9828c163d6cca027170d8696b5cfcee95
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-04 14:16:06 +0100

    ADD: add placeholder teams logic

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const nameField = document.getElementById('name');
 const roleField = document.getElementById('role');
 const imageField = document.getElementById('image');
 const addButton = document.getElementById('addMemberButton');
+const teamContainer = document.querySelector('.team-container');
 
 const data = [
 	{ name: 'Wayne Barnett', role: 'Founder & CEO', image: '../img/wayne-barnett-founder-ceo.jpg' },
@@ -26,4 +27,11 @@ const userTemplate = (name, role, image) => {
     `;
 };
 
+function showUser(teamData) {
+	for (const userObject in teamData) {
+		const user = teamData[userObject];
+		teamContainer.insertAdjacentHTML('beforeend', userTemplate(user.name, user.role, user.image));
+	}
+}
+showUser(data);
 console.log('js');
