2d382ffbb86ca0d3d5655f8e4d2b1474213d81c3
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-04 14:10:14 +0100

    get dom elements

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const nameField = document.getElementById('name');
+const roleField = document.getElementById('role');
+const imageField = document.getElementById('image');
+const addButton = document.getElementById('addMemberButton');
+
+const data = [
+	{ name: 'Wayne Barnett', role: 'Founder & CEO', image: '../img/wayne-barnett-founder-ceo.jpg' },
+	{ name: 'Angela Caroll', role: 'Chief Editor', image: '../img/angela-caroll-chief-editor.jpg' },
+	{ name: 'Walter Gordon', role: 'Office Manager', image: '../img/walter-gordon-office-manager.jpg' },
+	{ name: 'Angela Lopez', role: 'Social Media Manager', image: '../img/angela-lopez-social-media-manager.jpg' },
+	{ name: 'Scott Estrada', role: 'Developer', image: '../img/scott-estrada-developer.jpg' },
+	{ name: 'Barbara Ramos', role: 'Graphic Designer', image: '../img/barbara-ramos-graphic-designer.jpg' },
+];
+
+const userTemplate = (name, role, image) => {
+	return `
+    <div class="team-card">
+        <div class="card-image">
+            <img src="${image}" />
+        </div>
+        <div class="card-text">
+            <h3>${name}</h3>
+            <p>${role}</p>
+        </div>
+    </div>
+    `;
+};
+
+console.log('js');
